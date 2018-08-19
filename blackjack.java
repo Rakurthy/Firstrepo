fb799f21b09e8a1e37c489ad5f6db9996c3dea2c
Murthy <RM045269@cerner.net>
2018-08-19 21:56:53 +0530

    blackjack simple update

--- blackjack.java
+++ blackjack.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class blackjack {
 
@@ -51,13 +52,33 @@ public class blackjack {
 		System.out.println("Player " +player +" total is: " +Playertotal);
 		System.out.println("\n");
 		hm.put(Playertotal, "player"+player);
-		System.out.println(hm);
+		/*System.out.println(hm);*/
 	}
 	
-	}
+	 Set<Entry<Integer, String>> st = hm.entrySet();   
 	
-/*
-		if(Playertotal>Dealertotal)
+	/* for (Entry<Integer, String> me:st)
+     {
+		
+         System.out.print(me.getKey()+":");
+         System.out.println(me.getValue());
+     }*/
+	 
+	 Map.Entry<Integer, String> firstEntry = hm.entrySet().iterator().next();
+
+	 int largestKey = firstEntry.getKey();
+		String largestKeyValue = firstEntry.getValue();
+	 for (Map.Entry<Integer, String> map : hm.entrySet()) {
+		    int key = map.getKey();
+		    if (key > largestKey) {
+			largestKey = key;
+			largestKeyValue = map.getValue();
+		    }
+	 }
+		    System.out.println("Winning player     : " + largestKey);
+			System.out.println("Total card value: " + largestKeyValue);
+		    
+/*	 if(Playertotal>Dealertotal)
 		{
 			
 			System.out.println("Player " +player  +" wins");
@@ -69,5 +90,5 @@ public class blackjack {
 		*/
 	
 	}
-	
+}
 	
